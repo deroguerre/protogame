0504cee119eb4e9abdec1c3bdf11c32c0ce4284e
gui <deroguerre@gmail.com>
2019-03-19 18:15:51 +0100

    visual studio project

--- protogame/main.cpp
+++ protogame/main.cpp
@@ -15,13 +15,13 @@
 *   This example has been created using raylib 1.0 (www.raylib.com)
 *   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
 *
-*   Copyright (c) 2014 Ramon Santamaria (@raysan5)
+*   Copyright (c) 2013-2016 Ramon Santamaria (@raysan5)
 *
 ********************************************************************************************/
 
-#include "raylib.h"
+#include <raylib.h>
 
-int main(int argc, char* argv[])
+int main()
 {
 	// Initialization
 	//--------------------------------------------------------------------------------------
@@ -47,7 +47,7 @@ int main(int argc, char* argv[])
 
 		ClearBackground(RAYWHITE);
 
-		DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+		DrawText("Congrats! You created your first window!", 190, 200, 20, MAROON);
 
 		EndDrawing();
 		//----------------------------------------------------------------------------------
@@ -56,7 +56,7 @@ int main(int argc, char* argv[])
 	// De-Initialization
 	//--------------------------------------------------------------------------------------   
 	CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+	//--------------------------------------------------------------------------------------
 
 	return 0;
 }
