2525aa5df7eb2484bfd78c0a37d84db41745c1de
Oshariser <vince.avinenc@gmail.com>
2019-04-25 02:57:56 +0200

    Improve and clean World class

--- protogame/maps/world_map.tsx
+++ protogame/maps/world_map.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.3" name="overworld_tileset_grass" tilewidth="16" tileheight="16" tilecount="252" columns="12">
- <image source="../assets/overworld_tileset_grass.png" width="192" height="336"/>
+ <image source="../assets/worldmap_tileset.png" width="192" height="336"/>
 </tileset>
