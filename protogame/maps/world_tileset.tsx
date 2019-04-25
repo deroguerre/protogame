f57c86d4ee87dda89bb66002b3172da39fac900b
Oshariser <vince.avinenc@gmail.com>
2019-04-25 02:51:49 +0200

    Remove old files

--- protogame/maps/world_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="world_tileset" tilewidth="32" tileheight="32" tilecount="240" columns="20">
- <image source="../assets/0-tileset-x32.png" width="640" height="384"/>
-</tileset>
